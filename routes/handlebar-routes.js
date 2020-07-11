ef862acfa75bcc49e4f0223b2f2a1fc5059b171f
Sandra <sandra.alpha2010@gmail.com>
2020-07-11 12:53:46 -0700

    changing paths to use res.render instead

--- routes/handlebar-routes.js
+++ routes/handlebar-routes.js
@@ -1,5 +1,5 @@
 // Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
+//const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -10,7 +10,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/login", (req, res) => {
@@ -24,6 +24,9 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render(path.join(__dirname, "../public/members.html"));
   });
 };
+
+//change to res.render("signup")<---change this to index?
+//and grab data from database to fill in the different pages
