a3b4f22c32dc3328fbfa51d52025638572959769
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-07-14 21:14:43 -0700

    added routes and js to show member name info on hbs when ready

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,8 @@ $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.email);
+    //$("#email").text(data.email); figure out where in members hbs to grab
+    //$("#first-name").text(data.firstName);
+    //$("#last-name").text(data.lastName);
   });
 });
